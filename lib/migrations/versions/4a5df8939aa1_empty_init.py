7d05d44df66cc8d8e8d951f1a1724a619526d8c0
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-09-01 17:56:13 +0300

    lab passed

---
+++ lib/migrations/versions/4a5df8939aa1_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4a5df8939aa1
+Revises: 
+Create Date: 2023-09-01 17:49:38.031755
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4a5df8939aa1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
